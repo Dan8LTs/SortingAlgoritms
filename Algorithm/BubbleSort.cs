ebae2871f1b1835e763b3cf0b90ef814f7e2df69
Danil <danil.maskin@inbox.ru>
2021-04-12 17:30:10 +0300

    Добавьте файлы проекта.

---
+++ Algorithm/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class BubbleSort<T> : BaseAlgorithm<T> where T : IComparable
+    {
+        public override void Sort()
+        {
+            var count = Items.Count;
+
+            for(int v = 0; v < count; v++)
+            {
+                for (int i = 0; i < count - 1 - v; i++)
+                {
+                    var itemA = Items[i];
+                    var itemB = Items[i + 1];
+
+                    if (itemA.CompareTo(itemB) == 1)
+                    {
+                        Swop(i, i + 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SortingAlgoritms/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SortingAlgoritms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Algorithm;
+
+namespace SortingAlgoritms
+{
+    public partial class Main : Form
+    {
+        BaseAlgorithm<int> algorithm = new BubbleSort<int>();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(richTextBox1.Text, out int value))
+            {
+                algorithm.Items.Add(value);
+                label1.Text += " " + value;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            algorithm.Sort();
+
+            foreach(var item in algorithm.Items)
+            {
+                label2.Text += " " + item;
+            }
+        }
+    }
+}
